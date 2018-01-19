f2f468c58a47821afb0ac5b8b605644ef1ffd8cb
Thorsten Kurth <thorstenkurth@me.com>
2018-01-19 13:09:55 -0800

    fixed tiny bugs

--- run_scripts/run_cori_knl_craype-ml.sh
+++ run_scripts/run_cori_knl_craype-ml.sh
@@ -2,8 +2,8 @@
 #SBATCH -q premium
 #SBATCH -A nstaff
 #SBATCH -C knl
-#SBATCH -t 1:00:00
-#SBATCH -J cosmogan_horovod
+#SBATCH -t 4:00:00
+#SBATCH -J cosmogan_craype-ml
 
 #use custom craype-ml installation
 module use /global/homes/t/tkurth/custom_rpm
@@ -16,6 +16,10 @@ module load craype-ml-plugin-py2/1.1.0
 #add this to library path:
 export PYTHONPATH=$(pwd)/../networks:${PYTHONPATH}
 
+#better binding
+bindstring="numactl -C 1-67,69-135,137-203,205-271"
+#bindstring=""
+
 #run training
-srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 272 -u python -u ../networks/run_dcgan.py
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 272 -u ${bindstring} python -u ../networks/run_dcgan.py
 #srun -N 1 -n 1 -c 272 -u python ../networks/run_dcgan.py
