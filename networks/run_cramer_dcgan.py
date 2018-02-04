3e47b1df90c6203ff47d7814a7f2e8f10c36f7f8
Thorsten Kurth <thorstenkurth@me.com>
2018-02-03 23:22:35 -0800

    tweaking

--- networks/run_cramer_dcgan.py
+++ networks/run_cramer_dcgan.py
@@ -25,10 +25,10 @@ experiment = 'cramer_dcgan_cosmo_primary_256_200k_batchSize%i_learningRate_%0.6f
                                                                  ng_layers, gf_dim, df_dim, z_dim, numnodes, nodeid)
 
 command = 'python -m models.main --model cramer_dcgan --dataset cosmo --datafile %s '\
-          '--output_size %i --flip_labels %f --experiment %s '\
+          '--output_size %i --learning_rate %f --flip_labels %f --experiment %s '\
           '--epoch %i --batch_size %i --z_dim %i '\
           '--nd_layers %i --ng_layers %i --gf_dim %i --df_dim %i --save_every_step %s '\
-          '--data_format %s --transpose_matmul_b %s --verbose %s --num_inter_threads %i --num_intra_threads %i'%(datafile, output_size, flip_labels, experiment,\
+          '--data_format %s --transpose_matmul_b %s --verbose %s --num_inter_threads %i --num_intra_threads %i'%(datafile, output_size, learning_rate, flip_labels, experiment,\
                                                                                                                 epoch, batch_size, z_dim,\
                                                                                                                 nd_layers, ng_layers, gf_dim, df_dim, save_every_step,\
                                                                                                                 data_format, transpose_matmul_b, verbose, 2, 33)
