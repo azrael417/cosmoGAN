a6597fd0a584a808dce7e601dcf0cf64330cd51d
Thorsten Kurth <thorstenkurth@me.com>
2018-02-25 12:23:51 -0800

    added edison run script

---
+++ run_scripts/run_edison_horovod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -q regular
+#SBATCH -A m1759
+#SBATCH -C ivybridge
+#SBATCH -t 2:00:00
+#SBATCH -J cosmogan_horovod
+
+#set up python stuff
+module load python
+source activate thorstendl-edison-2.7
+
+#set library path
+export PYTHONPATH=$(pwd)/../networks
+
+#run training
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 48 -u python -u ../networks/run_dcgan_edison.py
