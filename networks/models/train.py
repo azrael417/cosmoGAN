c3d69e0dcfdd86557c5b3fe32a416b6503ea8962
adrianalbert <adrian.t.albert@gmail.com>
2018-03-15 15:49:42 -0700

    split validation code

--- networks/models/train.py
+++ networks/models/train.py
@@ -8,6 +8,8 @@ from utils import save_checkpoint, load_checkpoint
 import matplotlib
 matplotlib.use('Agg')
 
+from validation import *
+
 from scipy import stats
 
 
@@ -31,76 +33,6 @@ class decoder(object):
     example = 2* (tf.reshape(example,self.output_shape) - self.minval) / (self.maxval - self.minval) -1.
     return example
 
-
-def sample_tfrecords_to_numpy(tfrecords_filenames, img_size, n_samples=1000, normalization=None):
-
-    #init decoder
-    dec = decoder((img_size, img_size), normalization=normalization)
-
-    filenames = tf.placeholder(tf.string, shape=[None])
-    dataset = tf.data.TFRecordDataset(filenames)
-    dataset = dataset.map(lambda x: dec.decode(x))  # Parse the record into tensors.
-    dataset = dataset.repeat(1)  # Repeat the input indefinitely.
-    dataset = dataset.batch(n_samples)
-    iterator = dataset.make_initializable_iterator()
-    next_element = iterator.get_next()
-
-    # Initialize `iterator` with training data.
-    with tf.Session() as sess:
-      sess.run(iterator.initializer, 
-              feed_dict={filenames: tfrecords_filenames})
-      images = sess.run(next_element)
-      sess.close()
-      
-    return images
-
-def get_hist_bins(data, get_error=False):
-    y, x = np.histogram(data, bins=60, range=(-1.1,1.1))
-    x = 0.5*(x[1:]+x[:-1])
-    if get_error == True:
-        y_err = np.sqrt(y)
-        return x, y, y_err
-    else:
-        return x, y
-
-
-def compute_evaluation_stats(fake, test):
-  test_bins, test_hist = get_hist_bins(test)
-  fake_bins, fake_hist = get_hist_bins(fake)
-  return {"KS":stats.ks_2samp(test_hist, fake_hist)[1]}
-
-
-def plot_pixel_histograms(fake, test, dump_path="./", tag=""):
-  test_bins, test_hist, test_err = get_hist_bins(test, get_error=True)
-  fake_bins, fake_hist, fake_err = get_hist_bins(fake, get_error=True)
-  ks_test = stats.ks_2samp(test_hist, fake_hist)[1]
-
-  fig, ax = plt.subplots(figsize=(7,6))
-  #plot test
-  ax.errorbar(test_bins, test_hist, yerr=test_err, fmt='--ks', \
-  label='Test', markersize=7)
-
-  # plot generated
-  fake_label = 'GAN-' + tag if tag is not None else "GAN"
-  ax.errorbar(fake_bins, fake_hist, yerr=fake_err, fmt='o', \
-             label=fake_label, linewidth=2, markersize=6);
-
-  ax.legend(loc="best", fontsize=10)
-  ax.set_yscale('log');
-  ax.set_xlabel('Pixel Intensity', fontsize=18);
-  ax.set_ylabel('Counts (arb. units)', fontsize=18);
-  plt.tick_params(axis='both', labelsize=15, length=5)
-  plt.tick_params(axis='both', which='minor', length=3)
-  # plt.ylim(5e-10, 8*10**7)
-  # plt.xlim(-0.3,1.1)
-  plt.title('Pixels distribution (KS=%2.3f)'%ks_test, fontsize=16);
-
-  plots_dir = "%s/%s" % (dump_path, tag)
-  if not os.path.exists(plots_dir):
-      os.makedirs(plots_dir)
-
-  plt.savefig('%s/pixel_intensity.jpg'%plots_dir,bbox_inches='tight', format='jpg')
-  plt.savefig('%s/pixel_intensity.pdf'%plots_dir,bbox_inches='tight', format='pdf')
 
 def generate_samples(sess, dcgan, n_batches=20):
     z_sample = np.random.normal(size=(dcgan.batch_size, dcgan.z_dim))
