9f7b907b6e990087580b477adc64d4411101885d
Thorsten Kurth <tkurth@lbl.gov>
2018-03-22 06:23:42 +0100

    added daint run script

---
+++ run_scripts/run_daint_p100_horovod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J cosmogan_horovod
+#SBATCH -A g107
+#SBATCH -t 02:00:00
+#SBATCH -p normal
+#SBATCH -C gpu
+
+module unload PrgEnv-cray
+module load PrgEnv-gnu
+module load gcc/5.3.0
+module load cudatoolkit/8.0.61_2.4.3-6.0.4.0_3.1__gb475d12
+source activate tensorflow
+
+#add the models to the pythonpath
+export PYTHONPATH=$(pwd)/../networks
+
+export OMP_NUM_THREADS=12
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+export CRAY_CUDA_MPS=1
+export MPICH_RDMA_ENABLED_CUDA=1
+
+#run training
+srun -N ${SLURM_NNODES} -n ${SLURM_NNODES} -c 24 -u python -u ../networks/run_dcgan_daint.py --datapath
+
